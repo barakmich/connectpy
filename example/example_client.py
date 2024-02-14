89fe508ec254a757d048bbefb45eb0290ed4b5ca
Barak Michener <me@barakmich.com>
2024-02-13 20:35:26 -0800

    Remove google-based proto generator

--- example/example_client.py
+++ example/example_client.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser(description="Run a connect or GRPC server")
 parser.add_argument("--grpc", default=False, action="store_true")
 parser.add_argument("--hostport", default="127.0.0.1:5001")
 parser.add_argument("--msg", default="hello world")
+parser.add_argument("--json", default=False, action="store_true")
 
 
 async def main():
@@ -18,7 +19,7 @@ async def main():
     if args.grpc:
         channel = Channel(host=host, port=int(port))
     else:
-        channel = ConnectChannel(host=host, port=int(port))
+        channel = ConnectChannel(host=host, port=int(port), use_json=args.json)
     async with channel:
         svc = CapsServiceStub(channel)  # type: ignore
         resp = await svc.caps(caps_request=CapsRequest(msg=args.msg))
